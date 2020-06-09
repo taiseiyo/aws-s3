eae363697b38e94133cf9f6e052cb8db410329e0
taiseiyo <53172326+taiseiyo@users.noreply.github.com>
2020-06-10 01:00:21 +0900

    fix

--- aws-s3.sh
+++ aws-s3.sh
@@ -1,17 +1,17 @@
 #!/bin/bash 
-command=$1
-target1=$2
-target2=$3
+command=$1;shift
+target1=$1;shift
+target2=$1
 
 path=/home/ubuntu/Downloads/s3/
 
-if [ -n $1 -a -z $2 -a -z $3 ] ; then
+if [ -n $command -a -z "$target1" -a -z "$target2" ]; then
     ssh aws aws s3 $command
     
-elif [ -n $1 -a -n $2 -a -z $3 ] ;then 
+elif [ -n $command -a -n $target1 -a -z "$target2" ]; then 
     ssh aws aws s3 $command $target1
     
-elif [ -n $1 -a -n $2 -a -n $3 ] ;then
+elif [ -n $command -a -n $target1 -a -n $target2 ]; then
     scp $target1 aws:$path
     ssh aws aws s3 $command $path/$target1 $target2  
 
